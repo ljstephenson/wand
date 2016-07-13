3cd630d402a218e11b6986219bd3032547b92144
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2016-07-13 15:10:39 +0100

    make version number programmatically available

--- wand/__init__.py
+++ wand/__init__.py
@@ -1,3 +1,12 @@
 """
 WAND - Wavemeter Analysis 'N' Display
 """
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__package__)
+except DistributionNotFound:
+    print("Please install WAnD using pip for full functionality")
+    raise
+else:
+    VERSION = __package__ + '-' + __version__

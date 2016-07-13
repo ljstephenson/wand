3a07979b3a07914c6892abc362479bae394b048b
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2016-07-13 16:15:43 +0100

    actually get version number, not distribution

--- wand/__init__.py
+++ wand/__init__.py
@@ -4,7 +4,7 @@ WAND - Wavemeter Analysis 'N' Display
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    __version__ = get_distribution(__package__)
+    __version__ = get_distribution(__package__).version
 except DistributionNotFound:
     print("Please install WAnD using pip for full functionality")
     raise

7989cd57857def8c59a6146792c1fbe0fbaa56dc
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2016-10-10 14:37:56 +0100

    bump version number for interleaving feature

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ scripts = [
 
 setup(
     name='wand',
-    version='2.1.1',
+    version='2.2.0',
     url='https://github.com/ljstephenson/wand',
     author='Laurent Stephenson',
     packages=find_packages(),

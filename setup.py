b19a015cc820f02969bf52188a054a48264c98a8
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2017-02-08 17:24:36 +0000

    patch to allow conda package to build

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
+import sys
 
 scripts = [
     "wand_server=wand.server.main:main",
     "wand_client=wand.client.main:main",
 ]
 
+requirements=['json-rpc>=1.10']
+if '--conda' not in sys.argv:
+   requirements += ['influxdb>=2', 'PyDAQmx>=1']
+else:
+    index = sys.argv.index('--conda')
+    sys.argv.pop(index)  # Removes the '--conda'
+
 setup(
     name='wand',
-    version='2.1.3',
+    version='2.1.4',
     url='https://github.com/ljstephenson/wand',
     author='Laurent Stephenson',
     packages=find_packages(),
     entry_points={
         "console_scripts": scripts,
     },
-    install_requires=[
-        'influxdb>=2',
-        'json-rpc>=1.10',
-        'PyDAQmx>=1',
-        'quamash',
-    ],
+    install_requires=requirements,
     package_data={'wand': ['resources/*.svg', 'resources/*.json']},
     include_package_data=True
 )
